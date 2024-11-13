8238dce1f536a6825d8e1b998b2eb7c07c6a6231
fahimprito <ahmmedahmmed75@gmail.com>
2024-11-14 00:27:10 +0600

    basic setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header/Header";
+import Navbar from "../components/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Navbar></Navbar>
+            
+        </div>
+    );
+};
+
+export default Home;
